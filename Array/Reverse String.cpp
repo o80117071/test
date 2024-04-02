cf57b785bf274469c180131eba2630f9ab4c034b
o80117071 <o80117071@gmail.com>
2024-04-02 15:25:50 +0800

    Create Reverse String.cpp

---
+++ Array/Reverse String.cpp
@@ -0,0 +1,19 @@
+//時間複雜度 O(n)
+//空間複雜度 O(1)
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i = 0;
+        int j = s.size()-1;
+        while(i<j)
+        {
+            char tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+            
+            i++;
+            j--;
+        }
+    }
+};
